ee220a303b960dc8c00dd57f427dc76fe8ce06de
Jean-Philippe Jullien <jean-philippe.jullien@sage.com>
2016-06-29 15:49:45 +0200

    a

--- TestPowershell1.ps1
+++ TestPowershell1.ps1
@@ -1,5 +1,6 @@
 echo -------------------- 
 echo ZPowershell__Sleep
+Write-Error -Message "Error: Too many input values." -Category InvalidArgument
 Start-sleep 2
 get-process | out-file process.txt
 echo --------------------
