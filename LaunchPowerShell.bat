e041e8824bf0e4d3ab00ec2505b8b4ad55b2460a
Jean-Philippe Jullien <jean-philippe.jullien@sage.com>
2016-06-29 15:36:21 +0200

    a

--- LaunchPowerShell.bat
+++ LaunchPowerShell.bat
@@ -1,5 +1,6 @@
 echo LaunchPowerShell.bat Start
-powershell -ExecutionPolicy RemoteSigned -file TestPowershell1.ps1
+rem powershell -ExecutionPolicy RemoteSigned -file TestPowershell1.ps1
+powershell -file TestPowershell1.ps1
 set code=%errorlevel%
 echo '------------------ code  ' + %code%
 echo LaunchPowerShell.bat End
